23b283603b5561a36893536211a7f3e55b261ea0
Raul Ferraz <128731015+Raulberseker@users.noreply.github.com>
2023-04-26 15:41:08 -0300

    Create ex1app.js

---
+++ ex1app.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+    primeiroNome = "John";
+    sobrenome = "Doe";
+    idade = 30;
+    email = "johndoe@example.com";
+    classificacao = "Cliente Ouro";
+    empresa = "Acme Inc.";
+    receitaAnual = "$1 milhão";
+}
